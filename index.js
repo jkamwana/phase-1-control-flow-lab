fca280c9640c22502ea85ebad7996cf6d342d562
Joseph.K <joseph.kamwana@student.moringashool.com>
2023-06-18 08:53:59 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
+  it('gives customers a free sample if the ride is less than or equal to 400 feet', 
+  function(){
+    except(scuberGreetingForFeet(199)).to.equal('This one is on me!');
+  });
   // Write your code here!
 }
 
